49ec8c4f9dbef086ed80ebabc092720def2b0a9d
jin.ping <736252868@qq.com>
2022-10-28 11:05:36 +0800

    test

--- cache/lru_test.go
+++ cache/lru_test.go
@@ -1,6 +1,9 @@
 package lru
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 type String string
 
@@ -16,5 +19,38 @@ func TestGet(t *testing.T) {
 	}
 	if _, ok := lru.Get("key2"); !ok {
 		t.Logf("cache miss key2 failed")
+	}
+}
+
+func TestRemoveOldest(t *testing.T) {
+	k1, k2, k3 := "key1", "key2", "k3"
+	v1, v2, v3 := "value1", "value2", "v3"
+	cap := len(k1 + k2 + v1 + v2) //4 + 4 + 6 + 6 = 20 bytes
+	t.Logf("cap = %v", cap)
+	lru := New(int64(cap), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3)) //k1淘汰
+
+	if _, ok := lru.Get("key1"); !ok {
+		t.Logf("Removeoldest key1 occur, cur len = %d， cur bytes = %v, cur data = %v", lru.Len(), lru.nbytes, lru.cache)
+	}
+}
+
+func TestOnEvicted(t *testing.T) {
+	keys := make([]string, 0)
+	callback := func(key string, value Value) {
+		t.Logf("keys has append key = %s", key)
+		keys = append(keys, key)
+	}
+	lru := New(10, callback)
+	lru.Add("key1", String("123456"))
+	lru.Add("k2", String("k2"))
+	lru.Add("k3", String("k3"))
+	lru.Add("k4", String("k4"))
+	expect := []string{"key1", "k2"}
+
+	if reflect.DeepEqual(expect, keys) {
+		t.Logf("Call OnEvicted ok, expect key equals to %s", keys)
 	}
 }
